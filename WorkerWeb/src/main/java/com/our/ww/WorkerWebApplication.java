047654c18def0bde8d641530a481452013a36b03
devilasu <73906066+devilasu@users.noreply.github.com>
2025-01-10 19:43:43 +0900

    New Project Init

---
+++ WorkerWeb/src/main/java/com/our/ww/WorkerWebApplication.java
@@ -0,0 +1,13 @@
+package com.our.ww;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkerWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkerWebApplication.class, args);
+	}
+
+}
---
+++ WorkerWeb/src/test/java/com/our/ww/WorkerWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.our.ww;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkerWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
