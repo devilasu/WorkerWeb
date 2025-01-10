c55e467d29ce86b210944ed5a9f3654c19e2a547
devilasu <73906066+devilasu@users.noreply.github.com>
2025-01-10 21:45:15 +0900

    New config
    설정파일 추가 및 스프링시큐리티 및 설정 테스트

---
+++ WorkerWeb/src/main/java/com/our/ww/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.our.ww.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity(debug=true)
+public class SecurityConfig {
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+			.authorizeHttpRequests((authorize) -> authorize
+				.anyRequest().authenticated()
+			)
+			.httpBasic(Customizer.withDefaults())
+			.formLogin(Customizer.withDefaults());
+
+		return http.build();
+	}
+
+	@Bean
+	UserDetailsService userDetailsService() {
+		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+		manager.createUser(User.withUsername("user").password("1234").roles("USER").build());
+		//manager.deleteUser("user");
+		
+		return manager;
+	}
+}
